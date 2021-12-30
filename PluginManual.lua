c5a73fcfe4d656e0449b9244f36d57ed983404f5
brokenpencil09 <92695919+lolz59@users.noreply.github.com>
2021-12-29 17:25:45 -0800

    Update PluginManual.lua

--- PluginManual.lua
+++ PluginManual.lua
@@ -10,7 +10,9 @@
 --[[ Info
 The purpose of plugins are to easily share commands to friends
 In main.lua, the default command plugin is "csg4YPRb" and includes:
- > /gun "Remington 870"/"M9"/"AK47"/"M4A1"
+ > /gun "Remington 870"/"M9"/"AK47"/"M4A1" (Prison life only)
  > /kill [target]
  > /rejoin
+ > /goto [target]
+ > /speed [amount]
 --]]

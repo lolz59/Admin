d2a80e9bd1e46ab9b743b92bd38279ea6f7a6eec
brokenpencil09 <92695919+lolz59@users.noreply.github.com>
2021-12-29 17:26:09 -0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,17 @@
+--[[ TRZ Admin
+> Command template: https://raw.githubusercontent.com/lolz59/Admin/main/template.lua
+> Plugin instructions: https://raw.githubusercontent.com/lolz59/Admin/main/PluginManual.lua
+> If a section contains * in its title, do not delete anything within it.
+--]]
+
+-- *Settings
+Prefix = "/"
+
+-- *Main
+loadstring(game:HttpGet("https://pastebin.com/raw/FAN6Wn76"), true)
+
+-- Commands
+Import("csg4YPRb") -- Default commands
+
+-- *Command runner plugin
+Import("1ufPgGqs")

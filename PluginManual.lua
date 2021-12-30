5a51115366ad55282bcc8d87ee3e92d68846fae7
brokenpencil09 <92695919+lolz59@users.noreply.github.com>
2021-12-29 17:05:13 -0800

    Create PluginManual.lua

---
+++ PluginManual.lua
@@ -0,0 +1,16 @@
+--[[ How to make a plugin
+1.) Make a pastebin at https://pastebin.com
+2.) Make functions by using the template included in main.lua
+3.) Save the pastebin and go into raw
+4.) Copy the code after pastebin.com/raw/
+5.) Go to command plugins in main.lua
+6.) Import the plugin by using Import([The code you copied])
+--]]
+
+--[[ Info
+The purpose of plugins are to easily share commands to friends
+In main.lua, the default command plugin is "csg4YPRb" and includes:
+ > /gun "Remington 870"/"M9"/"AK47"/"M4A1"
+ > /kill [target]
+ > /rejoin
+--]]
